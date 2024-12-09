44de1cf066a650a0fd030104c7adf96c3c48e3fe
Ramon Melo <ramonmelo.com@gmail.com>
2024-12-09 15:01:54 -0300

    feat: implements the save of the first `item`

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-# Bibliotecas
-
 import os
 import time
-from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -13,14 +10,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-# load the .env variables
-load_dotenv()
-
 # access the variables from the dotEnv file
 login = os.getenv("LOGIN")
 senha = os.getenv("SENHA")
 url = os.getenv("URL")
-
 
 
 
@@ -29,32 +22,88 @@ driver = webdriver.Chrome(service=service)
 driver.get(url)
 
 
-#close the cookie option button
+
+try:
+    WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="adopt-accept-all-button"]')) # this text is the signal that the page is loaded
+    )
+except:
+    print("Botão de cookies não encontrado.")
+    driver.quit()
+
 time.sleep(3)
-cookie_button = driver.find_element(by=By.XPATH, value='//*[@id="adopt-accept-all-button"]')
-cookie_button.click()
+
+try: 
+    #close the cookie option button
+    cookie_button = driver.find_element(by=By.XPATH, value='//*[@id="adopt-accept-all-button"]')
+    cookie_button.click()
+    time.sleep(3)
+except NoSuchElementException:
+    print("Botão de cookies não encontrado.")
+except ElementNotInteractableException:
+    print("Botão de cookies encontrado, mas não pode ser interagido.")
+except Exception as e:
+    print(f"Ocorreu um erro inesperado: {e}")
 
 
-time.sleep(3)
+try:
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div[2]/div/div/div/div/section[1]/div/section/form/div[1]/div/div[2]/div/div/input')) # this text is the signal that the page is loaded
+    )
+except:
+    print("Botão de cookies não encontrado.")
+    driver.quit()
+
 
 #login
 
 login_box = driver.find_element(by=By.XPATH, value='//*[@id="app"]/div[2]/div/div/div/div/section[1]/div/section/form/div[1]/div/div[2]/div/div/input')
 login_box.send_keys(login)
-time.sleep(3)
+time.sleep(1)
 password_box = driver.find_element(by=By.XPATH, value='//*[@id="app"]/div[2]/div/div/div/div/section[1]/div/section/form/div[2]/div/div[2]/div/div/input')
 password_box.send_keys(senha)
-time.sleep(3)
-
-send_button = driver.find_element(by=By.XPATH, value='//*[@id="app"]/div[2]/div/div/div/div/section[1]/div/section/form/button')
-                                                    
+time.sleep(1)
+send_button = driver.find_element(by=By.XPATH, value='//*[@id="app"]/div[2]/div/div/div/div/section[1]/div/section/form/button')                                               
 send_button.click()
 
-time.sleep(12)
+#listings
+
+
+try:
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,'//*[@id="top-page"]/div/section[1]/div[1]/button')) # holds the program flow until this element is present
+    )
+except:
+    print("Botão de cookies não encontrado.")
+    driver.quit()
 
 driver.get('https://canalpro.grupozap.com/ZAP_OLX/0/listings')
 
-time.sleep(5)
+#####################################################################################
+
+#?pageNumber=2
+
+#//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[1]/div/section/div[2]/section[2]/section[2]/section[1]/ul/li[1]/a
+#//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[2]/div/section/div[2]/section[2]/section[2]/section[1]/ul/li[1]/a
+#//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[3]/div/section/div[2]/section[2]/section[2]/section[1]/ul/li[1]/a
+
+#page 2
+#//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[1]/div/section/div[2]/section[2]/section[2]/section[1]/ul/li[1]/a
+
+
+#//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[8]/div/section/div[2]/section[2]/section[2]/section[1]/ul/li[1]/a
+
+
+##############time.sleep(1000)
+try:
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[1]/div/section/div[2]/section[2]/section[2]/section[1]/div')) # holds the program flow until the edit button is present
+    )
+except:
+    print("Botão de cookies não encontrado.")
+    driver.quit()
+
+
 options = driver.find_element(by=By.XPATH, value='//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[1]/div/section/div[2]/section[2]/section[2]/section[1]/div')
 options.click()
 
@@ -63,122 +112,44 @@ time.sleep(10)
 editar = driver.find_element(by=By.XPATH, value='//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[1]/div/section/div[2]/section[2]/section[2]/section[1]/ul/li[1]/a')
 editar.click()
 
+time.sleep(10)
+try:
+    elemento = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,'/html/body/div[2]'))
+    )
+    print("Elemento encontrado. Tentando ocultá-lo...")
+    driver.execute_script("arguments[0].remove();", elemento)
+    print("Elemento ocultado com sucesso.")
+except Exception as e:
+    print(f"Erro ao tentar ocultar o elemento: {e}")
 
-# time.sleep(6)
 
-# salvar = driver.find_element(by=By.XPATH, value='//*[@id="top-page"]/div/div[1]/footer/div/div/button[2]/span')
+
+
+#//*[@id="category-0"]/div[2]/span[1]/label
+#//*[@id="572"]
+#//*[@id="app"]/div[2]/div[1]/div[2]/div/div/a/div/svg
+
+# time.sleep(8)
+# //*[@id="adopt-controller-button"]/svg/path[2]
+# //*[@id="adopt-controller-button"]/svg
+# //*[@id="adopt-controller-button"]/svg
+#//*[@id="adopt-controller-button"]
+#/html/body/div[2]
+
+
+salvar = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="top-page"]/div/div[1]/footer/div/div/button[2]'))
+)
+salvar.click()
+
+
+# salvar = driver.find_element(by=By.XPATH, value='//*[@id="top-page"]/div/div[1]/footer/div/div/button[2]')
 # salvar.click()
 
-
-time.sleep(100)
-
+time.sleep(1000)
 
 
 
 
 
-#//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[1]/div/section/div[2]/section[2]/section[2]/section[1]/div
-
-
-
-# time.sleep(5)
-# options = driver.find_element(by=By.XPATH, value='//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[1]/div/section/div[2]/section[2]/section[2]/section[1]/div')
-# options.click()
-
-# time.sleep(3)
-
-# editar = driver.find_element(by=By.XPATH, value='//*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[1]/div/section/div[2]/section[2]/section[2]/section[1]/ul/li[1]/a')
-# editar.click()
-# time.sleep(10)
-
-# save = driver.find_element(by=By.XPATH, value='//*[@id="top-page"]/div/div[1]/footer/div/div/button[2]')
-# save.click()
-
-# time.sleep(100)
-
-
-
-
-
-
-
-
-# ver anuncios //*[@id="performance"]/section/div[7]/div/section/div[2]/div/button
-# tres pontinhos: //*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[1]/div/section/div[2]/section[2]/section[2]/section[1]/div
-# editar: //*[@id="top-page"]/section/div[4]/div/div/section/div/div/div/div[1]/div/section/div[2]/section[2]/section[2]/section[1]/ul/li[1]/a
-#salvar: //*[@id="top-page"]/div/div[1]/footer/div/div/button[2]
-
-
-
-# # LOGIN
-# autentica = driver.find_element(
-#     by=By.XPATH, value="//*[@id='autenticacao:loginEmpresa']")
-# autentica.send_keys(login)
-
-# senha_input = driver.find_element(
-#     by=By.XPATH, value="//*[@id='autenticacao:senhaEmpresa']")
-# senha_input.send_keys(senha)
-# entrar = driver.find_element(
-#     by=By.XPATH, value="//*[@id='autenticacao:entrarEmpresa']")
-# entrar.click()
-
-
-
-# banner = WebDriverWait(driver, 20).until(EC.visibility_of_element_located(By.XPATH, value ='//*[@id="adopt-accept-all-button"]'))
-# banner.click()
-# time.sleep(200)
-
-#banner = WebDriverWait(driver, 20).until(by=By.XPATH, value='//*[@id="adopt-accept-all-button"]')
-
-
-# search_box = WebDriverWait(driver, 20).until(
-#     EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div[2]/div/div/div/div/section[1]/div/section/form/div[1]/div/div[2]/div/div/input'))
-# )
-
-#search_box.send_keys("Texto de busca")
-
-
-#search_box = driver.find_element(by=By.XPATH, value='//*[@id="app"]/div[2]/div/div/div/div/section[1]/div/section/form/div[1]/div/div[2]/div/div/input')
-# search_box.click()
-#time.sleep(20)
-
-# # LOGIN
-# autentica = driver.find_element(
-#     by=By.XPATH, value="//*[@id='autenticacao:loginEmpresa']")
-# autentica.send_keys(login)
-
-# senha_input = driver.find_element(
-#     by=By.XPATH, value="//*[@id='autenticacao:senhaEmpresa']")
-# senha_input.send_keys(senha)
-# entrar = driver.find_element(
-#     by=By.XPATH, value="//*[@id='autenticacao:entrarEmpresa']")
-# entrar.click()
-
-
-# # Aguarda até que a página de login seja processada (espera um elemento da próxima página)
-# try:
-#     WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Todos os direitos reservados')]")) # this text is the signal that the page is loaded
-#     )
-# except:
-#     print("Erro no login ou elemento não encontrado.")
-#     driver.quit()
-
-# # Navega para a página desejada
-# driver.get("http://sigec.crea-pi.org.br/sigec/servicosOnline/art.jsf") 
-
-# # Pausa para visualização
-
-# time.sleep(2)
-
-# fechar_banner = driver.find_element(
-#     by=By.XPATH, value='//*[@id="j_idt39"]/div[1]/a/span')
-# fechar_banner.click()
-
-
-
-
-# NOMEL = driver.find_element(by=By.XPATH, value='//*[@id="j_idt70"]/span[2]')
-# NOMEL.click()
-
-# time.sleep(600)
